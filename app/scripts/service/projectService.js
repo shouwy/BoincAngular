eb60dbe84b22d532152b80a69eb62d394b4d737b
shouwy <thierry.freres1@gmail.com>
2015-07-07 15:50:35 +0200

    Del ngTable for projectService

--- app/scripts/service/projectService.js
+++ app/scripts/service/projectService.js
@@ -39,46 +39,4 @@ angular.module('boincApp')
           isArray : true
         }
       });
-
-    function filterData(data, filter){
-      return $filter('filter')(data, filter);
-    }
-
-    function orderData(data, params){
-      return params.sorting() ? $filter('orderBy')(data, params.orderBy()) : filteredData;
-    }
-
-    function sliceData(data, params){
-      return data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-    }
-
-    function transformData(data,filter,params){
-      return sliceData( orderData( filterData(data,filter), params ), params);
-    }
-
-/*    var service = {
-      cachedProject : [],
-      getListProject : function($defer, params, filter) {
-        if (service.cachedProject.length > 0){
-          console.log('Using Cached Data');
-          var filteredData = filterData(service.cachedProject, filter);
-          var transformedData = sliceData(orderData(filteredData, params), params);
-          params.total(filteredData.length);
-          $defer.resolve(transformedData);
-        } else {
-          $http.get(APP.baseUrl + 'project/list').success(
-            function (resp) {
-              console.log('Fetching Data');
-              angular.copy(resp, service.cachedProject);
-              params.total(resp.length);
-              var filteredData = $filter('filter')(resp, filter);
-              var transformedData = transformData(resp, filter, params);
-
-              $defer.resolve(transformedData);
-            }
-          );
-        }
-      }
-    };
-    return service;*/
   });
